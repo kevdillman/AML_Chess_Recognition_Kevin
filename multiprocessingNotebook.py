5d09208eda645312a2eba5a3d9d47e1e78b0254b
JJGeither <jjgeither@gmail.com>
2024-04-24 20:21:34 -0400

    Fix Parallel Image Processing

--- multiprocessingNotebook.py
+++ multiprocessingNotebook.py
@@ -1,11 +1,32 @@
-import os
-
+from skimage import color, exposure
+from skimage.feature import hog
 from skimage.io import imread
 from skimage.util import view_as_blocks
 import numpy as np
+import multiprocessing as mp
+import os
 import sys
 
 def runt(fileName, src, fen_from_filename, fen_from_position, NUM_SPACES_PER_BOARD):
+    
+    def HogTransform(img):
+        first_image_gray = color.rgb2gray(img)
+
+        fd, hog_image = hog(
+            first_image_gray,
+            orientations=8,
+            pixels_per_cell=(8, 8),
+            cells_per_block=(1, 1),
+            visualize=True,
+            block_norm='L2-Hys',
+            feature_vector=True
+        )
+
+        # Rescale histogram for better display
+        hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
+        hog_image_uint8 = (hog_image_rescaled * 255).astype(np.uint8)
+        return hog_image_uint8
+
     print('Hello from the child process')
     # flush output
     sys.stdout.flush()
@@ -23,13 +44,9 @@ def runt(fileName, src, fen_from_filename, fen_from_position, NUM_SPACES_PER_BOA
         
         for patch in patches:
             patchType = fen_from_position(boardPosition, fenString)
-            if patchType == ' ':
-                if spaceTilesStored >= NUM_SPACES_PER_BOARD:
-                    continue
-                spaceTilesStored += 1
             
-            #hog_features = HogTransform(patch)
-            data['data'].append(patch)
+            hog_features = HogTransform(patch)
+            data['data'].append(hog_features)
             data['fenstring'].append(patchType)
             boardPosition += 1
     
